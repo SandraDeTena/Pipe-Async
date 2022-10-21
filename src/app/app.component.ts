8cd4193fcd558fa28eac4e7eb9ff22f441ce89b3
Sandra <dtena90@gmail.com>
2022-10-21 09:02:44 +0200

    Crear la promesa dentro del TS de app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostsService } from './services/posts.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesAsync';
+
+  //AsyncPipe
+  promPost: Promise<any[]>;
+
+  constructor(private PostsService: PostsService) {
+
+    this.promPost = this.PostsService.getAll();
+  }
+
+  ngOnInit() { }
 }
+
+
+
+
