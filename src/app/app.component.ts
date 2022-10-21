b4a1492945bc170f3a51fbec33ba54c5e25f81df
Sandra <dtena90@gmail.com>
2022-10-21 12:12:52 +0200

    currencyPipe
    Está es para formatear el símbolo del € y modificar el número del precio.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,17 @@ import { PostsService } from './services/posts.service';
 export class AppComponent {
   title = 'pipesAsync';
 
+  //currencyPipe => Se encarga de formatear(y por tanto de mostrar) tema de precios, por ejemplo en una tienda porque tiene un precio especifico.
+  precio: number;
+
   //AsyncPipe
-  promPost: Promise<any[]>;
+  // promPost: Promise<any[]>;
 
   constructor(private PostsService: PostsService) {
+    //currentyPipe
+    this.precio = 198.87;
 
-    this.promPost = this.PostsService.getAll();
+    // this.promPost = this.PostsService.getAll();
   }
 
   ngOnInit() { }
