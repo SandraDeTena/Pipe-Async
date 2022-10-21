26acd4b0982ea51955704552c2ce87a380bc6f15
Sandra <dtena90@gmail.com>
2022-10-21 09:18:17 +0200

    Cambiar de idioma a español la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//Cambiar a la aplicación el idioma a Español
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
 
 //Importar el HTTPCLIENT
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -16,7 +21,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID, useValue: 'es-ES'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
