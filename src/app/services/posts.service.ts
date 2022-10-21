0cbb6d32fe54618141dfabfe0de5861fab202b1c
Sandra <dtena90@gmail.com>
2022-10-21 08:54:41 +0200

    Crear el método para recuperar todos - getAll

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+//Para importar los métodos para hacer las peticiones
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  //Método para recuperar todos
+  getAll(): Promise<any[]> {
+    return this.httpClient.get<any[]>('https://jsonplaceholder.typicode.com/posts').toPromise();
+  }
 }
