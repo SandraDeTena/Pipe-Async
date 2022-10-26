1ccab544338e9b47577afe87baac27ff660b665f
Sandra <dtena90@gmail.com>
2022-10-26 08:23:37 +0200

    Crear un servicio | está comentado

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 //Para importar los métodos para hacer las peticiones
 import { HttpClient } from '@angular/common/http';
 
+const URL_BASE = 'https://rickandmortyapi.com/api/character/'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +12,9 @@ export class PostsService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getCharacter(n: number) {
+    return this.httpClient.get('${URL_BASE}${n}');
+  }
   //Método para recuperar todos
   // getAll(): Promise<[]> {
   //   return this.httpClient.get<[]>('https://jsonplaceholder.typicode.com/posts').toPromise();
