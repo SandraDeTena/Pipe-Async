7ac4d8e25c00f4f5ba179ecf41d579cf71b5dc96
Sandra <dtena90@gmail.com>
2022-10-26 08:25:18 +0200

    PercentPipe
    => Convertir un número que a priori no es un porcentaje convertirlo a %.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, VERSION, OnDestroy, OnInit } from '@angular/core';
+import { map, Observable, Subscription, of } from 'rxjs';
 import { PostsService } from './services/posts.service';
+import { delay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,9 @@ import { PostsService } from './services/posts.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  name = 'Angular' + VERSION.major;
+
   title = 'pipesAsync';
 
   //currencyPipe => Se encarga de formatear(y por tanto de mostrar) tema de precios, por ejemplo en una tienda porque tiene un precio especifico.
@@ -18,11 +24,18 @@ export class AppComponent {
   //DatePipe
   fechaActual: Date;
 
+  //PercentPipe => Convertir un número que a priori no es un porcentaje convertirlo a %.
+  numAleatorio: number;
+
+  //JSONPipe =>Obtener de un objeto un poco más complejo una representación de string.
+  // estudiante: any;
+
 
   //AsyncPipe
   // promPost: Promise<any[]>;
 
   constructor(private PostsService: PostsService) {
+
     //currentyPipe
     this.precio = 198.87;
 
@@ -32,15 +45,23 @@ export class AppComponent {
     //DatePipe
     this.fechaActual = new Date();
 
+    //PercentPipe
+    this.numAleatorio = Math.random();
+
+    //JSONPipe =>Obtener de un objeto un poco más complejo una representación de string.
+    // this.estudiante = {
+    //   nombre: 'Roberto',
+    //   apellidos: 'López',
+    //   notas: [3.5, 4.8, 7, 8.3]
+    // }
+
     // this.promPost = this.PostsService.getAll();
   }
+
 
   ngOnInit() {
     //CRONOMETRO
     setInterval(() => this.fechaActual = new Date(), 1000);
   }
+
 }
-
-
-
-
