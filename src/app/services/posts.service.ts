9747b6bdebe5ac46b5d7e5caabd205232488992b
Sandra <dtena90@gmail.com>
2022-11-01 18:42:37 +0100

    Creación de la petición getAll a través de Observable

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 //Para importar los métodos para hacer las peticiones
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/iPost';
 
 
 @Injectable({
@@ -13,7 +15,7 @@ export class PostsService {
   constructor(private httpClient: HttpClient) { }
 
   //Método para recuperar todos
-  getAll(): Promise<any[]> {
-    return this.httpClient.get<any[]>('https://jsonplaceholder.typicode.com/posts').toPromise();
+  getAll(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
   }
 }
